79c443eda501f0aaa43b65d258ad0dae05a9e64d
Erik Tierney <tierney@adobe.com>
2008-01-11 11:14:19 -0500

    Fix linux build.  Forgot to update the makefile after I removed a source file.

--- shell/manifest.mk
+++ shell/manifest.mk
@@ -23,7 +23,6 @@ shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/FileInputStream.cpp \
   $(curdir)/StringBuilderClass.cpp \
   $(curdir)/SystemClass.cpp \
-  $(curdir)/TypedArrayClass.cpp \
   $(curdir)/../extensions/DictionaryGlue.cpp \
   $(curdir)/../extensions/JavaGlue.cpp \
   $(curdir)/../extensions/SamplerScript.cpp \
