589d8439dd66008e2d554a7aecb359fdb5022dba
Lars T Hansen <lhansen@adobe.com>
2008-12-16 14:43:26 -0800

    Fix 469882: Disable RTTI and C++ exceptions (r+stejohns)

--- configure.py
+++ configure.py
@@ -107,11 +107,11 @@ if MMGC_THREADSAFE:
 os, cpu = config.getTarget()
 
 if config.getCompiler() == 'GCC':
-    APP_CXXFLAGS = "-fstrict-aliasing -Wextra -Wall -Wno-reorder -Wno-switch -Wno-invalid-offsetof -Wsign-compare -Wunused-parameter -fmessage-length=0 "
+    APP_CXXFLAGS = "-fstrict-aliasing -Wextra -Wall -Wno-reorder -Wno-switch -Wno-invalid-offsetof -Wsign-compare -Wunused-parameter -fmessage-length=0 -fno-rtti -fno-exceptions "
     if config.getDebug():
-        APP_CXXFLAGS += "-frtti -fexceptions "
+        APP_CXXFLAGS += ""
     else:
-        APP_CXXFLAGS += "-Wuninitialized -fno-rtti -fno-exceptions "
+        APP_CXXFLAGS += "-Wuninitialized "
     DEBUG_CXXFLAGS += "-g "
 elif config.getCompiler() == 'VS':
     if cpu == "arm":
