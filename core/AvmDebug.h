c9ac3a8f708bc84279b66635c44c408f34b51ddc
Edwin Smith <edwsmith@adobe.com>
2009-01-06 15:55:17 -0500

    move abort() call to AvmDebugUnix.cpp, to fix winmobile build

--- core/AvmDebug.h
+++ core/AvmDebug.h
@@ -80,7 +80,6 @@ namespace avmplus
 			#else
 				AvmDebugMsg(message, true);
 			#endif
-			abort(); // in case above handler didn't exit, we will
 		}
 
 		inline void _AvmAssertMsg(int32 assertion, const char* message) {
