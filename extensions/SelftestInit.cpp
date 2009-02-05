02cbd9d9abe5c82a3c06cff64824561dc48bd9f4
Lars T Hansen <lhansen@adobe.com>
2009-02-05 15:21:25 +0100

    Regenerated selftest runner

--- extensions/SelftestInit.cpp
+++ extensions/SelftestInit.cpp
@@ -38,8 +38,16 @@
 #include "avmshell.h"
 namespace avmplus {
 #ifdef AVMPLUS_SELFTEST
+extern void create_avmplus_basics(AvmCore* core);
+#if defined AVMPLUS_PEEPHOLE_OPTIMIZER
+extern void create_avmplus_peephole(AvmCore* core);
+#endif
 extern void create_mmgc_basics(AvmCore* core);
 void SelftestRunner::createGeneratedSelftestClasses() {
+create_avmplus_basics(core);
+#if defined AVMPLUS_PEEPHOLE_OPTIMIZER
+create_avmplus_peephole(core);
+#endif
 create_mmgc_basics(core);
 }
 #endif // AVMPLUS_SELFTEST
