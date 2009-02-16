059f57e9e0f40ae14086f582c6791285888205f6
Steven Johnson <stejohns@adobe.com>
2009-02-16 13:59:26 -0800

    Previous fix broke Sampler, because it required AbstractFunction::name to not make do any allocation. Restructured the way CallStackNode (both real and fake) work to avoid having to allocate the names at sample-gathering time (we can defer it until we actually use the sample).
    Problem went unnoticed because the Mac XCode project included AvmDebugGeneric.cpp rather than AvmDebugMac.cpp, so the asserts were being swallowed -- fixed this too.
    
    Also changed configure.py to have MMGC_INTERIOR_PTRS default to False rather than True, to match the Windows and Mac IDE builds.

--- core/StackTrace.cpp
+++ core/StackTrace.cpp
@@ -56,7 +56,7 @@ namespace avmplus
 		m_core			= env->core();
 		m_env			= env;
 		m_next			= m_core->callStack; m_core->callStack = this;
-		m_envname		= env->method->getMethodName();
+		m_fakename		= NULL;
 		m_depth			= m_next ? (m_next->m_depth + 1) : 1;
 		m_eip			= eip;     // ptr to where the current instruction pointer is stored
 		m_filename		= NULL;
@@ -76,7 +76,7 @@ namespace avmplus
 	{
 		// careful, core and/or name can be null
 		m_env			= NULL;
-		m_envname		= name;
+		m_fakename		= name;
 		if (name)
 		{
 			AvmAssert(core != NULL);
