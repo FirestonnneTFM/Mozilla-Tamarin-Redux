df204b1d13175af04ade3071c7407e12e439116c
Lars T Hansen <lhansen@adobe.com>
2009-02-18 10:12:52 +0100

    Reverted removal

--- platform/mac/manifest.mk
+++ platform/mac/manifest.mk
@@ -39,5 +39,6 @@ avmplus_CXXSRCS := $(avmplus_CXXSRCS) \
   $(curdir)/AvmDebugMac.cpp \
   $(curdir)/../unix/DateUnix.cpp \
   $(curdir)/../unix/MathUtilsUnix.cpp \
+  $(curdir)/NativeFunctionMac.cpp \
   $(curdir)/../unix/OSDepUnix.cpp \
   $(NULL)
