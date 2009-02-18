23e0947e0148afecd2eb3bb2541a1b828f1aad88
Lars T Hansen <lhansen@adobe.com>
2009-02-18 09:57:48 +0100

    Removed obsolete Mac files

--- platform/mac/manifest.mk
+++ platform/mac/manifest.mk
@@ -39,6 +39,5 @@ avmplus_CXXSRCS := $(avmplus_CXXSRCS) \
   $(curdir)/AvmDebugMac.cpp \
   $(curdir)/../unix/DateUnix.cpp \
   $(curdir)/../unix/MathUtilsUnix.cpp \
-  $(curdir)/NativeFunctionMac.cpp \
   $(curdir)/../unix/OSDepUnix.cpp \
   $(NULL)
