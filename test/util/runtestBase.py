43c14fcfb43730975fe22975a70027102be3cecd
Dan Schaffer <dschaffe@adobe.com>
2009-02-23 10:48:05 -0500

    bug 461758: date fixes for CET, 2 fixes where GMT- was failing. r=lhansen also a useful change to runTestBase.py to rebuild any abc where the source (.as) is newer than the .abc file.  So any testcase fixes are automatically rebuilt.  applies to both acceptance and performance tests.

--- test/util/runtestBase.py
+++ test/util/runtestBase.py
@@ -840,6 +840,9 @@ class RuntestBase:
         if self.forcerebuild and isfile(testName):
             os.unlink(testName)
         
+        if isfile(testName) and getmtime(ast)>getmtime(testName):
+        	self.verbose_print("%s has been modified, recompiling" % ast)
+        	os.unlink(testName)
         if not isfile(testName):
             self.compile_test(ast)
             if not isfile(testName):
