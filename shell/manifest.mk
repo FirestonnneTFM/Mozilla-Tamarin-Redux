1e599f7dc1168fecebc33ea3bb6cd059d0fff474
Steven Johnson <stejohns@adobe.com>
2009-04-07 11:44:29 -0700

    Move VMPI implementation into a new toplevel directory: files were moved around, and dependencies on avmshell were removed from MacPortUtils and WinPortUtils. (Also note that direct calls to printf() crept back in to these files; IMHO we should address this in subsequent fixes.) (r=rishah)

--- shell/manifest.mk
+++ shell/manifest.mk
@@ -74,19 +74,15 @@ shell_CXXSRCS := $(shell_CXXSRCS) \
 ifeq (windows,$(TARGET_OS))
 shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/avmshellWin.cpp \
-  $(curdir)/WinDebugUtils.cpp \
   $(curdir)/WinFile.cpp \
-  $(curdir)/WinPortUtils.cpp \
   $(NULL)
 endif
 
 ifeq (darwin,$(TARGET_OS))
 shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/avmshellMac.cpp \
-  $(curdir)/MacDebugUtils.cpp \
   $(curdir)/PosixFile.cpp \
   $(curdir)/PosixPartialPlatform.cpp \
-  $(curdir)/PosixPortUtils.cpp \
   $(NULL)
 endif
 
@@ -95,8 +91,6 @@ shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/avmshellUnix.cpp \
   $(curdir)/PosixFile.cpp \
   $(curdir)/PosixPartialPlatform.cpp \
-  $(curdir)/PosixPortUtils.cpp \
-  $(curdir)/UnixDebugUtils.cpp \
   $(NULL)
 endif
 
@@ -105,8 +99,6 @@ shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/avmshellUnix.cpp \
   $(curdir)/PosixFile.cpp \
   $(curdir)/PosixPartialPlatform.cpp \
-  $(curdir)/PosixPortUtils.cpp \
-  $(curdir)/UnixDebugUtils.cpp \
   $(NULL)
 endif
 
