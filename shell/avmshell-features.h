ed22ecbd8c11eda69812ece936b4d9780ad00336
Brent Baker <brbaker@adobe.com>
2009-08-26 14:02:56 -0400

    Bug 512270: update how the AVMFEATURE_MEMORY_PROFILER feature is configured (r+stejohns) - fix how the string tests run on windows mobile when iterationsMobile is not passed into timetest() - make sure to pass in the --notimecheck switch to the acceptance testsuite in the 'smokes' configuration files

--- shell/avmshell-features.h
+++ shell/avmshell-features.h
@@ -161,10 +161,10 @@
   #define AVMFEATURE_OVERRIDE_GLOBAL_NEW 0
 #endif
 
-#if defined AVMPLUS_MAC && !(defined MMGC_PPC && defined MMGC_64BIT)
+#if AVMSYSTEM_MAC && !(AVMSYSTEM_PPC && AVMSYSTEM_64BIT)
 	#define AVMFEATURE_MEMORY_PROFILER 1
-#elif defined AVMPLUS_WIN32 && !defined AVMPLUS_ARM // note, does not require DEBUG
-    #define AVMFEATURE_MEMORY_PROFILER 1
+#elif AVMSYSTEM_WIN32 && !AVMSYSTEM_ARM // note, does not require DEBUG
+	#define AVMFEATURE_MEMORY_PROFILER 1
 #else
 	#define AVMFEATURE_MEMORY_PROFILER 0
 #endif
