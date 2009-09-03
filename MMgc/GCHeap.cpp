70b091cae46c19cd00f8f8ec2f97bf6bd1ebb232
Tommy Reilly <treilly@adobe.com>
2009-09-03 13:20:31 -0400

    Fix OOM locking issue by releasing FixedAlloc lock when we get a new chunk (r=lhansen)

--- MMgc/GCHeap.cpp
+++ MMgc/GCHeap.cpp
@@ -1600,11 +1600,6 @@ namespace MMgc
 		if(ef != NULL)
 		{
 			VMPI_lockRelease(&m_spinlock);
-			// we might be jumping over a FixedMalloc lock, unlock
-			for (int i=0; i<FixedMalloc::kNumSizeClasses; i++) {
-				VMPI_lockTestAndAcquire(&FixedMalloc::GetFixedMalloc()->m_allocs[i]->m_spinlock);
-				VMPI_lockRelease(&FixedMalloc::GetFixedMalloc()->m_allocs[i]->m_spinlock);
-			}
 			longjmp(ef->jmpbuf, 1);
 		}
 		GCAssertMsg(false, "MMGC_ENTER missing!");
