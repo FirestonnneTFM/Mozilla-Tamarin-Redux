2925766f9cc56ce44da442aeea32b42c203dce80
Brent Baker <brbaker@adobe.com>
2009-11-03 11:30:17 -0500

    Bug 526229: REVERT Update the remote deployment scripts to handle being passed revision and rev-hash (r=dschaffe)

--- build/buildbot/slaves/all/run-acceptance-release-interp-arm.sh
+++ build/buildbot/slaves/all/run-acceptance-release-interp-arm.sh
@@ -100,7 +100,7 @@ cd $basedir/build/buildbot/slaves/scripts
 ../all/util-acceptance-clean.sh
 
 echo "Setting up the device with build #$change"
-../all/avmshell-arm-setup.sh $change:$changeid
+../all/avmshell-arm-setup.sh $change
 
 
 cd $basedir/test/acceptance
--- build/buildbot/slaves/all/util-calculate-change.sh
+++ build/buildbot/slaves/all/util-calculate-change.sh
@@ -41,28 +41,15 @@ cwd=`pwd`
 cd $basedir
 if [ -d .hg ];
 then
-    loc=`expr index "$1" \:`
-    # nothing passed in
-    if [ "$1" == "" ]; then
-        change=`hg identify -n | awk -F+ '{print $1}'`
-        echo "change number not passed using tip: $change"
-        changeid=`hg log -r $change | head -n 1 | awk -F: '{print $3}'`
-        
-    # only revision passed in
-    elif [ $loc = 0  ]; then
-        change=$1
-        echo "change number passed: $change"
-        changeid=`hg log -r $change | head -n 1 | awk -F: '{print $3}'`
-    
-    # revision and changeid passed in
-    else
-        change=${1:0:$loc-1}
-        changeid=${1:$loc}
-        echo "change and changeid number passed: $change $changeid"
-    fi
-    
+    change=$1
+    test "$change" = "" && {
+      change=`hg identify -n | awk -F+ '{print $1}'`
+      echo "change number not passed using tip: $change"
+    }
+    change=`echo $change | awk -F: '{print $1}'`
+
+    changeid=`hg log -r $change | head -n 1 | awk -F: '{print $3}'`
     echo "running $platform build for change: $change changeid: $changeid"
-    
 
 else
     change=$1
