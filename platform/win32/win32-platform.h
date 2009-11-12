62a3f2e3d9af9e04705013edc662be5709379350
Rick Reitmaier <rreitmai@adobe.com>
2009-11-12 11:33:51 -0800

    Duplicate code in win32-platform.h removed (r+me)

--- platform/win32/win32-platform.h
+++ platform/win32/win32-platform.h
@@ -227,11 +227,6 @@ typedef unsigned __int64	uint64_t;
     #define NJ_NO_VARIADIC_MACROS
 #endif
 
-#if defined(_MSC_VER) && _MSC_VER < 1400 && defined(FEATURE_NANOJIT)
-    #define NJ_NO_VARIADIC_MACROS
-#endif
-
-
 /**
 * Type defintion for an opaque data type representing platform-defined spin lock 
 * @see VMPI_lockInit(), VMPI_lockAcquire()
