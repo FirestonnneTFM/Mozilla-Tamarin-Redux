2db45121892db0fd8efc75669a1b870059b97245
Steven Johnson <stejohns@adobe.com>
2009-11-16 10:24:06 -0800

    fix crashiness in Sampler (r=edwsmith, bug=528757)

--- extensions/SamplerScript.h
+++ extensions/SamplerScript.h
@@ -79,7 +79,7 @@ namespace avmplus
 
 #ifdef DEBUGGER
 	private:		
-		static ClassClosure* getType(SamplerObjectType sot, const void *obj);
+		static ClassClosure* getType(Toplevel* toplevel, SamplerObjectType sot, const void *obj);
 		
 		friend class SampleIterator;
 		static ScriptObject* makeSample(ScriptObject* self, const Sample& sample);
