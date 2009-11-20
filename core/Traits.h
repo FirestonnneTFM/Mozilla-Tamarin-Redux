9b598597c70ab700bf2896a2edb6c5943f2ad98d
Edwin Smith <edwsmith@adobe.com>
2009-11-20 07:01:02 -0500

    Implements new subtypeof algorithm (bug 528291 sr=stejohns+)
    New, faster subtypeof algorithm.
    
    See implementation comments in Traits.h
    
    This patch is a drop in replacement for the old subtypeof().
    Subsequent patches will:
    
    * correct unbounded recursion in Traits::countInterfaces and resolveSignatures
    * provide inlineable fast specialized variants of subtypeof for primary types,
    final types, known interfaces, etc.

--- core/Traits.h
+++ core/Traits.h
@@ -124,25 +124,19 @@ namespace avmplus
 			MethodInfo* f;
 		};
 		
-		struct InterfaceInfo
-		{
-			Traits* t;
-		};
-		
 	private:
 		TraitsBindings(Traits* _owner, 
 							TraitsBindingsp _base, 
 							MultinameHashtable* _bindings,
 							uint32_t _slotCount, 
-							uint32_t _methodCount, 
-							uint32_t _interfaceCapacity);
+							uint32_t _methodCount);
 
 	public:
 		static const uint32_t MAX_SLOT_OFFSET = (1U << 31) - 1;
 
 	public:
 		
-		static TraitsBindings* alloc(MMgc::GC* gc, Traits* _owner, TraitsBindingsp _base, MultinameHashtable* _bindings, uint32_t slotCount, uint32_t methodCount, uint32_t interfaceCount);
+		static TraitsBindings* alloc(MMgc::GC* gc, Traits* _owner, TraitsBindingsp _base, MultinameHashtable* _bindings, uint32_t slotCount, uint32_t methodCount);
 
 		void buildSlotDestroyInfo(MMgc::GC* gc, FixedBitSet& slotDestroyInfo, uint32_t slotAreaCount, uint32_t sizeOfSlotArea) const;
 
@@ -150,7 +144,6 @@ namespace avmplus
 		uint32_t getSlotOffset(uint32_t i) const;
 		// if you need SST you probably want offset too
 		SlotStorageType calcSlotAddrAndSST(uint32_t i, void* pin, void*& pout) const;
-		Traitsp getInterface(uint32_t i) const;
 		MethodInfo* getMethod(uint32_t i) const;
 		Binding findBinding(Stringp key) const;
 		Binding findBinding(Stringp name, Namespacep ns) const;
@@ -165,12 +158,8 @@ namespace avmplus
 		Binding valueAt(int32_t index) const;
 
 	private:
-		bool FASTCALL subtypeof(Traitsp t) const;
 		SlotInfo* getSlots();
 		const SlotInfo* getSlots() const;
-
-		InterfaceInfo* getInterfaces();
-		const InterfaceInfo* getInterfaces() const;
 
 		BindingMethodInfo* getMethods();
 		const BindingMethodInfo* getMethods() const;
@@ -179,7 +168,6 @@ namespace avmplus
 
 		void setMethodInfo(uint32_t i, MethodInfo* f);
 
-		bool addOneInterface(Traitsp intf);
 		bool checkOverride(AvmCore* core, MethodInfo* virt, MethodInfo* over) const;
 		bool checkLegalInterfaces(AvmCore* core) const;
 		void fixOneInterfaceBindings(Traitsp ifc, const Toplevel* toplevel);
@@ -188,9 +176,9 @@ namespace avmplus
 		public:		const Traitsp					owner;
 		public:		const TraitsBindingsp			base; 
 		private:	MultinameHashtable* const		m_bindings;
+		private:    Traits**						m_interfaces;
 		public:		const uint32_t					slotCount;			// including slots in our base classes
 		public:		const uint32_t					methodCount;		// including methods in our base classes
-		public:		const uint32_t					interfaceCapacity;	// cap, not count: will contain null entries
 		private:	uint32_t						m_slotSize;			// size of slot area in bytes, including base classes
 		// plus extra at end
 	// ------------------------ DATA SECTION END
@@ -256,6 +244,7 @@ namespace avmplus
 	class Traits : public MMgc::GCObject 
 	{
 		friend class TraitsBindings;	// for m_sizeofInstance
+		friend class InterfaceIterator;
 		#if defined FEATURE_NANOJIT
 		friend class CodegenLIR;
 		#endif
@@ -301,8 +290,6 @@ namespace avmplus
 
 		TraitsPosPtr traitsPosStart() const;
 		TraitsPosPtr skipToInstanceInitPos(TraitsPosPtr pos) const;
-		void countInterfaces(const Toplevel* toplevel, List<Traitsp, LIST_GCObjects>& seen);
-		bool addInterfaces(TraitsBindings* tb, const Toplevel* toplevel);
 		Binding getOverride(TraitsBindingsp basetb, Namespacep ns, Stringp name, int tag, const Toplevel *toplevel) const;
 
 		void addVersionedBindings(MultinameHashtable* bindings,
@@ -351,7 +338,15 @@ namespace avmplus
 		TraitsMetadatap getTraitsMetadata();
 		bool containsInterface(Traitsp t); // legacy name
 		bool subtypeof(Traitsp t);
-		
+		static Traits** allocSupertypeList(MMgc:: GC*, uint32_t size);
+
+	private:
+		void build_primary_supertypes();
+		void build_secondary_supertypes();
+		bool FASTCALL secondary_subtypeof(Traitsp t);	// slow path called by subtypeof()
+		bool isPrimary() const;
+		uint32_t countNewInterfaces(List<Traitsp, LIST_GCObjects>& seen);
+
 	public:
 		void genDefaultValue(uint32_t value_index, uint32_t slot_id, const Toplevel* toplevel, Traits* slotType, CPoolKind kind, AbcGen& gen) const;
 		void genInitBody(const Toplevel* toplevel, AbcGen& gen);
@@ -410,9 +405,106 @@ namespace avmplus
 		// essential for efficient building of IMT thunks.
 		bool implementsNewInterfaces() const;
 
+		// Data Structures for fast subtype checking (edwsmith 11/17/09):
+		// subtypeof(S,T) is frequently used by the (often implicit) "is" and "coerce"
+		// operators and therefore is critical to optimize.
+		//
+		// We have the following data structure in each Traits:
+		//   * primary_supertypes: a fixed-sized array of the first several base classes
+		//   * MAX_PRIMARY_SUPERTYPE: a tunable policy constant, currently 8 from testing
+		//   * secondary_supertypes: a possibly shared null terminated array of any
+		//     remaining base classes, plus all interfaces implemented by this Traits
+		//   * supertype_cache: a one-entry positive-hit cache
+		//   * supertype_neg_cache: a one-entry negative-hit cache
+		//   * supertype_offset: if this traits is a shallowly-inherited class, holds
+		//     the byte offset of this traits in any subclass's primary_supertypes array.
+		//     otherwise, holds the byte offset of supertype_cache.
+		//
+		// secondary_supertypes is shared as follows:
+		//   * if a class is primary with no interfaces, we share an empty-list singleton
+		//   * if a class is primary and doesn't add interfaces, list is shared with base class.
+		//   * if a class is a leaf class and doesn't add interfaces, we share the list between
+		//     the non-leaf base class and each other non-interface-adding leaf subclass
+		//
+		// supertype_offset cleverly encodes whether the traits is "primary" and expected
+		// to be in subclass's primary_supertypes list, or "secondary" and expected to be
+		// in the secondary_supertypes list.  When it points to the cache, this type is
+		// a secondary type.  Using this encoding allows step 1 (below) to be fast.
+		//
+		// primary_supertypes and secondary_supertypes are initialized in the Traits
+		// constructor.  primary_supertypes is never modified.  secondary_supertypes can
+		// be replaced once when a base class's list is shared by one or more leaf-subclasses.
+		// (see build_primary_supertypes() and build_secondary_supertype()).
+		//
+		// In a large app with thousands of classes and hundreds of interfaces,
+		// The combined memory of the shared secondary_supertypes lists is often
+		// <1% of the overall traits memory.  The "big knob" affecting memory vs performance
+		// is the MAX_PRIMARY_SUPERTYPE policy constant.
+		//
+		// the operation subtypeof(S,T) that tests whether S is a subtype of T is:
+		//   1. if S[T.offset] == T, return true
+		//   2. if T.offset != offsetof(supertype_cache), return false
+		//   3. if S.supertype_neg_cache == T, return false
+		//   4. if S == T, return true
+		//   5. if T in S.secondary_supertypes, set S.supertype_cache == T and return true
+		//   6. else, set S.supertype_neg_cache == T and return false.
+		//
+		// These tests are ordered by frequency based on empirical testing.
+		//
+		// step 1 combines the ordinary base-class test when T is primary with 
+		// the cache-hit test when T is secondary.  Step 2 exits fast when T is primary.
+		//
+		// when T is a known primary class, steps 1 & 2 collapse into a simple
+		// load and compare.
+		// 
+		// when T is a known interface or deeply inherited class, step 1 & 3 (both cache tests)
+		// become two loads+compares and step 2 can be skipped.
+		//
+		// Step 4 allows us to omit "self" from secondary_supertypes, which allows more
+		// sharing of that list.  Step 4 can be skipped when T is a known interface.
+		// When T is final, only step 4 is needed; this is not common in practice but
+		// worth noting for jit-compilation.
+		//
+		// supertype_neg_cache is a hidden pointer because when S is *not* a subtype of
+		// T, then S and T are unrelated and we do not want the edge S->T to pin T in memory.
+		// However, in subtype_cache, and primary/secondary_supertypes[], entries *are* all
+		// supertypes of S and therefore the extra references do not uniquely pin T in memory.
+		//
+		// primary_supertypes[] does not have a write barrier because it is initialized in
+		// the constructor before other memory is allocated, like base.
+		// 
+		// more ideas to optimize:
+		// 
+		// The final search in step 5 is a linear null terminated search and is used
+		// well under 1% of the time in a range of apps tested.  If necessary it could
+		// be further optimized by a) moving found items to the front; b) padding to 
+		// the next power of two and using a quadradic probe; c) comparing several items
+		// at once using SIMD instructions (essentially a fast strchr operation on pointers)
+		//
+		// we could save one load per subtypeof() operation by moving all these
+		// structures to VTable.  Emperically, there are about 1.2 VTables per Traits,
+		// so we'd expect to see a slight increase in overhead to save that load-per-test.
+		//
+		// to save memory or better handle deep class heirarchies we can tune MAX_PRIMARY_SUPERTYPE.
+		// the value 8 was picked after emperical testing on a range of Flash and Flex applications.
+		//
+		// The "base" field is redundant; if accessing base is not frequent we can
+		// encapsulate access and get it from either primary_supertypes or seconary_supertypes.
+		// (this can be O(1) for secondary_supertypes if base is always the 1st or 2nd entry).
+		// we can even walk up the class tree using secondary_supertypes and primary_supertypes
+		// in sequence.  (best to encapsulate this in an iterator class).
+
+		// size of the fixed-sized primary supertype list
+		private:	static const int		MAX_PRIMARY_SUPERTYPE = 8;
+
 	// ------------------------ DATA SECTION BEGIN
 	public:		AvmCore* const			core;		// @todo remove, can get from pool->core
 	public:		Traits* const			base;		// Pointer to the base traits; that is, the traits of the base class
+	private:	DWB(Traits*)			m_supertype_cache;	  // 1-entry cache for subtypeof=true
+	private:	MMgc::GCHiddenPointer<Traits*>
+	                                    m_supertype_neg_cache;  // 1-entry cache for subtypeof=false (hidden to avoid pinning)
+	private:	Traits* 				m_primary_supertypes[MAX_PRIMARY_SUPERTYPE]; // contains the first several base classes, written with explicit WB's
+	private:	DWB(Traits**)			m_secondary_supertypes;
 	public:		PoolObject* const		pool;		// The constant pool owning this definition. never null.
 	public:		Traits*					itraits;	// if this type is a factory, itraits is non-null and points to the type created by this factory.
 	private:	DRCWB(Namespacep)		_ns;			// The namespace of the class described by this traits object
@@ -435,7 +527,7 @@ namespace avmplus
 	public:		uint8_t					builtinType;				// BuiltinType enumeration -- only need 5 bits but stored in byte for faster access
 	private:	const uint8_t			m_posType;					// TraitsPosType enumeration -- only need 3 bits but stored in byte for faster access
 	private:	uint8_t					m_bindingCapLog2;			// if nonzero, log2 of the cap needed for bindings
-	private:	uint8_t					m_interfaceCapLog2;			// if nonzero, log2 of the cap needed for interfaces
+	private:    uint8_t					m_supertype_offset;			// if this traits is primary, == offset in primary_supertypes array; otherwise == offset of supertype_cache
 	// 7 bits follow
 	private:	uint32_t				m_needsHashtable:1;			// If true, the class needs a hash table. Typically true for dynamic classes, but will be false for XML
 	private:	uint32_t				linked:1;					// set once signature types have been resolved */
@@ -452,13 +544,12 @@ namespace avmplus
 	// iterates over each interface supertype of the given traits.
 	class InterfaceIterator
 	{
-		const TraitsBindings* tb;
-		uint32_t i;
+		Traits** st;
 	public:
 		InterfaceIterator(Traits*);
 		InterfaceIterator(const TraitsBindings*);
-		bool hasNext();
-		Traits* next();
+		bool hasNext();		// returns true if there are more interfaces
+		Traits* next();		// get the next one (moves iterator forward)
 	};
 }
 
