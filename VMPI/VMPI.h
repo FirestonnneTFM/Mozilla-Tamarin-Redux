b7e8003acef9b2098704fb00ffd176c78c0ebc45
Edwin Smith <edwsmith@adobe.com>
2010-01-05 14:47:11 -0500

    Remove sint8 legacy typedef since it's already unused (r=me)

--- VMPI/VMPI.h
+++ VMPI/VMPI.h
@@ -113,7 +113,7 @@ typedef int32_t		int32;
 //typedef int16_t    sint16;
 typedef uint8_t		uint8;
 //typedef int8_t     int8;
-typedef int8_t		sint8;
+//typedef int8_t     sint8;
 typedef uintptr_t	uintptr;
 //typedef intptr_t   sintptr;
 typedef uint8_t		byte;
