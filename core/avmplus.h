b1762fb9aa807b57836c3c7b3875e82e39e5600d
Lars T Hansen <lhansen@adobe.com>
2010-01-19 13:46:23 -0500

    For 531250 -  [Speed] Tune ScriptObject initialization: clean up newInstance (r+edwsmith)

--- core/avmplus.h
+++ core/avmplus.h
@@ -334,6 +334,7 @@ namespace avmplus
 #include "AbcParser-inlines.h"
 #include "atom-inlines.h"
 #include "AvmCore-inlines.h"
+#include "ClassClosure-inlines.h"
 #include "Coder-inlines.h"
 #include "instr-inlines.h"
 #include "MethodEnv-inlines.h"
