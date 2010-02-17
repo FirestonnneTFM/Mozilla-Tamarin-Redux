065c26b646599cc8e001e138d5211aeb8bdaa06e
Edwin Smith <edwsmith@adobe.com>
2010-02-17 09:46:09 -0500

    clean up whitespace, add modelines (r=edwsmith)

--- core/TraitsIterator.h
+++ core/TraitsIterator.h
@@ -1,3 +1,5 @@
+/* -*- Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*- */
+/* vi: set ts=4 sw=4 expandtab: (add to ~/.vimrc: set modeline modelines=5) */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -41,52 +43,52 @@
 
 namespace avmplus
 {
-	class TraitsIterator
-	{
-	public:
-		TraitsIterator(Traits * _traits) :
-			traits(_traits ? _traits->getTraitsBindings() : NULL),
-			index(0)
-		{
-		}
+    class TraitsIterator
+    {
+    public:
+        TraitsIterator(Traits * _traits) :
+            traits(_traits ? _traits->getTraitsBindings() : NULL),
+            index(0)
+        {
+        }
 
-		bool getNext(Stringp& key, Namespacep& ns, Binding& value)
-		{
-			if (index == -1 || !traits)
-			{
-				return false;
-			}
-			while ((index = traits->next(index)) == 0)
-			{
-				// ascend to base traits
-				traits = traits->base;
-				if (!traits)
-				{
-					// no more traits, bail out.
-					index = -1;
-					return false;
-				}
-				// restart iteration on base traits
-				index = 0;
-			}
-			key   = traits->keyAt(index);
-			ns    = traits->nsAt(index);
-			value = traits->valueAt(index);
-			return true;
-		}
+        bool getNext(Stringp& key, Namespacep& ns, Binding& value)
+        {
+            if (index == -1 || !traits)
+            {
+                return false;
+            }
+            while ((index = traits->next(index)) == 0)
+            {
+                // ascend to base traits
+                traits = traits->base;
+                if (!traits)
+                {
+                    // no more traits, bail out.
+                    index = -1;
+                    return false;
+                }
+                // restart iteration on base traits
+                index = 0;
+            }
+            key   = traits->keyAt(index);
+            ns    = traits->nsAt(index);
+            value = traits->valueAt(index);
+            return true;
+        }
 
-		Traits* currentTraits()
-		{
-			// this value changes as we walk up the traits chain
-			if (traits) return traits->owner;
-			return NULL;
-		}
+        Traits* currentTraits()
+        {
+            // this value changes as we walk up the traits chain
+            if (traits) return traits->owner;
+            return NULL;
+        }
 
-	private:
-		// this is stack-allocated so we must not use DWB()
-		TraitsBindingsp traits;
-		int index;
-	};
+    private:
+        // this is stack-allocated so we must not use DWB()
+        TraitsBindingsp traits;
+        int index;
+    };
 }
 
 #endif /* __avmplus_TraitsIterator__ */
