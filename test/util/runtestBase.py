26ca53ae410940385d613afcc7033e3512a26104
Chris Peyer <cpeyer@adobe.com>
2010-02-18 15:53:38 -0800

    Add error handling to determineconfig (r=cpeyer)

--- test/util/runtestBase.py
+++ test/util/runtestBase.py
@@ -363,28 +363,34 @@ class RuntestBase:
                 self.vmtype = 'release'
         elif not self.runSource and not self.rebuildtests:
             (f,err,exitcode) = self.run_pipe('%s' % self.avm)
-            # determine avmshell type
-            if re.search('debug-debugger',f[1]):
-                self.vmtype = 'debugdebugger'
-            elif re.search('release-debugger',f[1]):
-                self.vmtype = 'releasedebugger'
-            elif re.search('debug',f[1]):
-                self.vmtype = 'debug'
-            elif re.search('release',f[1]):
-                self.vmtype = 'release'
-            else:   # try to determine vmtype by filename
-                vm = splitext(split(self.avm)[1])[0]
-                if '_sd' in vm:
+            try:
+                # determine avmshell type
+                if re.search('debug-debugger',f[1]):
                     self.vmtype = 'debugdebugger'
-                elif '_s' in vm:
+                elif re.search('release-debugger',f[1]):
                     self.vmtype = 'releasedebugger'
-                elif '_d' in vm:
+                elif re.search('debug',f[1]):
                     self.vmtype = 'debug'
-                else:
+                elif re.search('release',f[1]):
                     self.vmtype = 'release'
+                else:   # try to determine vmtype by filename
+                    vm = splitext(split(self.avm)[1])[0]
+                    if '_sd' in vm:
+                        self.vmtype = 'debugdebugger'
+                    elif '_s' in vm:
+                        self.vmtype = 'releasedebugger'
+                    elif '_d' in vm:
+                        self.vmtype = 'debug'
+                    else:
+                        self.vmtype = 'release'
             
-            # get the build number and hash
-            self.avmversion = self.getAvmVersion(txt=f[1])
+                # get the build number and hash
+                self.avmversion = self.getAvmVersion(txt=f[1])
+            except:
+                # Error getting shell info
+                self.vmtype = 'unknown'
+                self.avmversion = 'unknown'
+            
             
             f = ' '.join(f)
             # determine if api versioning switch is available
