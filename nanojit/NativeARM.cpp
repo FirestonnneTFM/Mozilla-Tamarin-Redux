92f5809f9a91809e7f2cff89e0d32a5731dfcc57
Nicholas Nethercote <nnethercote@mozilla.com>
2010-03-23 15:05:47 -0700

    Bug 517910 - NJ: add more alias-set annotations to LIR so as to improve CSEing of loads.  r=edwsmith.

--- nanojit/NativeARM.cpp
+++ nanojit/NativeARM.cpp
@@ -109,9 +109,8 @@ Assembler::CountLeadingZeroes(uint32_t data)
     // ARMv4 anyway.
     NanoAssert(_config.arm_arch >= 5);
 
-#if defined(__ARMCC__) && (__ARMCC_VERSION >= 230000)
-
-    // ARMCC can do this with an intrinsic except ARMCC 2.2.x which is used by Symbian
+#if defined(__ARMCC__)
+    // ARMCC can do this with an intrinsic.
     leading_zeroes = __clz(data);
 
 // current Android GCC compiler incorrectly refuses to compile 'clz' for armv5
