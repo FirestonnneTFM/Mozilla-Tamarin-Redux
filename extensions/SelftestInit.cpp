44f0908cb1f4e811293d8d8398e12a6e559f03bd
Tommy Reilly <treilly@adobe.com>
2010-04-02 10:55:20 -0400

    merge

--- extensions/SelftestInit.cpp
+++ extensions/SelftestInit.cpp
@@ -49,6 +49,7 @@ extern void create_mmgc_bugzilla_543560(AvmCore* core);
 #endif
 extern void create_mmgc_basics(AvmCore* core);
 extern void create_mmgc_dependent(AvmCore* core);
+extern void create_mmgc_gcheap(AvmCore* core);
 #if defined VMCFG_WORKERTHREADS
 extern void create_mmgc_threads(AvmCore* core);
 #endif
@@ -64,6 +65,7 @@ create_mmgc_bugzilla_543560(core);
 #endif
 create_mmgc_basics(core);
 create_mmgc_dependent(core);
+create_mmgc_gcheap(core);
 #if defined VMCFG_WORKERTHREADS
 create_mmgc_threads(core);
 #endif
--- shell/manifest.mk
+++ shell/manifest.mk
@@ -72,6 +72,7 @@ shell_CXXSRCS := $(shell_CXXSRCS) \
   $(curdir)/../extensions/ST_mmgc_dependent.cpp \
   $(curdir)/../extensions/ST_mmgc_threads.cpp \
   $(curdir)/../extensions/ST_mmgc_543560.cpp \
+  $(curdir)/../extensions/ST_mmgc_gcheap.cpp \
   $(NULL)
   
   
