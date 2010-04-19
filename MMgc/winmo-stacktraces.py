df92b1ac197468e9a631edb7b250d5e85bc540ff
Tommy Reilly <treilly@adobe.com>
2010-04-19 14:02:29 -0400

    Fix MMgc's modelines

--- MMgc/winmo-stacktraces.py
+++ MMgc/winmo-stacktraces.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- Mode: Python; indent-tabs-mode: nil; tab-width: 4 -*-
+# vi: set ts=4 sw=4 expandtab: (add to ~/.vimrc: set modeline modelines=5)
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
