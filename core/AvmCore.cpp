6d61ad072e0fc8a9ecb4b611e86a1a988dcb6366
Felix S Klock II <fklockii@localhost>
2010-09-24 09:53:13 -0400

    Bug 558863: avoid exn throws from x in bytearray + regression tests (r=stejohns)

--- core/AvmCore.cpp
+++ core/AvmCore.cpp
@@ -4893,6 +4893,7 @@ return the result of the comparison ToPrimitive(x) == y.
             bugzilla513018 = 1;     // parseFloat accepts illegal number syntax
             bugzilla524122 = 1;     // Incorrect optimization for integers in numeric sort
             bugzilla526662 = 1;     // XMLParser stops at NUL char
+            bugzilla558863 = 1;     // in operator on bytearray throws exception for non-natural number
             bugzilla585791 = 1;     // String.localeCompare with a null String object returns 0
         }
     }
