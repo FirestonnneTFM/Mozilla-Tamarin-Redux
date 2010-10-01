b86b5b445a8253ab1e863a3b3c8f9adf5d212969
Edwin Smith <edwsmith@adobe.com>
2010-10-01 16:27:35 -0400

    Fix ARM bustage from 575848.
    ARM gcc has a different binary format for member function pointers,
    which we could handle with a bit more work.  #ifdef out for now.

--- core/MethodEnv.cpp
+++ core/MethodEnv.cpp
@@ -751,7 +751,7 @@ namespace avmplus
 
 #ifdef DEBUG
 
-#ifdef __GNUC__
+#if defined(__GNUC__) && !defined(VMCFG_ARM)
     // Ensure that if a native class object overrides ClassClosure::construct(),
     // then itraits->hasCustomConstruct must be true.  Conversely if it does not
     // override construct(), hasCustomConstruct must be false (lest we lose out
