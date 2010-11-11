acccb320fb6263f707708a6263a70532e5246cde
Dan Schaffer <dschaffe@adobe.com>
2010-11-11 09:58:03 -0500

    bug 611312: whitespace cleanup in test, remove tabs in cases where space/tabs were mixed, prepare for python3.x support (r=dschaffe)

--- test/util/runtestBase.py
+++ test/util/runtestBase.py
@@ -858,10 +858,10 @@ class RuntestBase:
                 shutil.copyfile(abcfile, outabc)
                 self.js_print('AOT compilation of %s' % (outabc))
 
-				# t = ("--timeout=%d" % self.testTimeOut) if self.testTimeOut > 0 else ""
-				cmd = '%s -Xshell -Xoutput %s %s %s %s' % (os.path.join(self.aotsdk, 'bin/pfi'), output, self.aotextraargs, " ".join(extraabcs), outabc)
-				self.js_print(cmd)
-				(f,err,exitcode) = self.run_pipe(cmd)
+                # t = ("--timeout=%d" % self.testTimeOut) if self.testTimeOut > 0 else ""
+                cmd = '%s -Xshell -Xoutput %s %s %s %s' % (os.path.join(self.aotsdk, 'bin/pfi'), output, self.aotextraargs, " ".join(extraabcs), outabc)
+                self.js_print(cmd)
+                (f,err,exitcode) = self.run_pipe(cmd)
 
                 for line in f:
                     self.js_print(("file '%s'>>> " % abcfile) + line.strip())
