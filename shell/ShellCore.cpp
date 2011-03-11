e0ae68a15cacde33e04d155e9b23e1c9f9b722c7
Steven Johnson <stejohns@adobe.com>
2011-03-11 15:24:05 -0800

    Bug 638998 followup: NativeBaseClass construction needs to be skipped in -Dverifyonly mode; applying for the second time as change 6067:4bd8f4b4fa01 accidentally reverted this change, sigh (r=me again)

--- shell/ShellCore.cpp
+++ shell/ShellCore.cpp
@@ -205,7 +205,12 @@ namespace avmshell
         //handleActionPool(shellPool, shell_toplevel, shell_codeContext);
         shell_toplevel->shellClasses = prepareBuiltinActionPool<shell_toplevelClassManifest>(shellPool, shell_toplevel, shell_codeContext);
 
-        shell_toplevel->shellClasses->get_NativeBaseClass()->constructObject();
+#ifdef VMCFG_VERIFYALL
+        if (!config.verifyonly)
+#endif
+        {
+            shell_toplevel->shellClasses->get_NativeBaseClass()->constructObject();
+        }
 
         return shell_toplevel;
     }
