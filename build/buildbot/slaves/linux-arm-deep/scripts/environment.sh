f8b4bca8ea43234cec764aa2201e292d4e576b59
Brent Baker <brbaker@adobe.com>
2011-07-26 07:54:34 -0400

    Bug 653484: Add OSR config compile and testing into the deep phase of the build (p=jsudduth,brbaker r=brbaker)

--- build/buildbot/slaves/linux-arm-deep/scripts/environment.sh
+++ build/buildbot/slaves/linux-arm-deep/scripts/environment.sh
@@ -49,12 +49,17 @@ export shell_extension=
 
 export platform=linux
 
+export CXX=arm-none-linux-gnueabi-g++
+export CC=arm-none-linux-gnueabi-gcc
+export AR=arm-none-linux-gnueabi-ar
+export LD=arm-none-linux-gnueabi-ld
+
 export shell_release=avmshell_neon_arm
 export shell_release_debugger=avmshell_neon_arm
 export shell_debug=avmshell_neon_arm_d
 export shell_debug_debugger=avmshell_neon_arm_d
 
-export ssh_proc_names="avmshell_neon_arm avmshell_neon_arm_d"
+export ssh_proc_names="avmshell_neon_arm avmshell_neon_arm_d avmshell_neon_arm_osr"
 
 # Override this, default is avmshell* and since this slave runs on a machine
 # with other slaves, the process cleaner /can/ find avmshell processes, 
