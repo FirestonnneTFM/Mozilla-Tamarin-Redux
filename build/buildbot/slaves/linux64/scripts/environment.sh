d38dd649b57699550668f2ec57717be78f5c2b3f
Brent Baker <brbaker@adobe.com>
2011-08-23 09:07:23 -0400

    Bug n/a: Need to specify the exact shell names to try and kill if they are running now that linux32 and 64 are running on the same machine. Without this change the buildslaves try and kill the others running shell processes (r=brbaker)

--- build/buildbot/slaves/linux64/scripts/environment.sh
+++ build/buildbot/slaves/linux64/scripts/environment.sh
@@ -55,3 +55,9 @@ export shell_debug=${shell_debug}_64
 export shell_release_debugger=${shell_release_debugger}_64
 export shell_debug_debugger=${shell_debug_debugger}_64
 export shell_selftest=${shell_selftest}_64
+
+# List of processes that should NEVER be running when the build is not
+# currently running any tests. This list of process will be killed if the
+# process is found. Process must not contain extension as cygwin will return
+# the process without the extension. Used in all/util-process-clean.sh
+export proc_names="avmshell.*_64$"
