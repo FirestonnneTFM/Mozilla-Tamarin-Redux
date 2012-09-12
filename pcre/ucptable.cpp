5a3a04f4c8a49b9cdd601730ae58f8ac23efe822
Dan Schaffer <Dan.Schaffer@adobe.com>
2012-09-12 13:06:22 -0700

    Bug 716548: Update license in tamarin source code to MPL2 (r=irichter) Part 2 of 4
    CL@1111603

--- pcre/ucptable.cpp
+++ pcre/ucptable.cpp
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /* This source module is automatically generated from the Unicode
 property table. See internal.h for a description of the layout. */
 
