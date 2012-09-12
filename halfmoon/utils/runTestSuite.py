9ebe26cb3b48f7345e3c7bc0aace24277e746b55
Dan Schaffer <Dan.Schaffer@adobe.com>
2012-09-12 13:06:22 -0700

    Bug 716548: Update license in tamarin source code to MPL2 (r=irichter) Part 2 of 4
    CL@1111603

--- halfmoon/utils/runTestSuite.py
+++ halfmoon/utils/runTestSuite.py
@@ -2,6 +2,10 @@
 # -*- Mode: Python; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4 -*-
 # vi: set ts=4 sw=4 expandtab:
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import sys
 import os
 import string
